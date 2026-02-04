e9f0f8a96eb6cf57c9bcc59334fda1d5ab0c8aa3
igor <igorcrispim154@gmail.com>
2026-02-04 10:51:33 -0300

    teste

---
+++ src/main/java/br/kge/eti/OSApiAaplication/OsApiAaplicationApplication.java
@@ -0,0 +1,13 @@
+package br.kge.eti.OSApiAaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsApiAaplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsApiAaplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/kge/eti/OSApiAaplication/OsApiAaplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.kge.eti.OSApiAaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiAaplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
